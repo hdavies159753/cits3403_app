1608b2b334e366abfb91ed7a6f3c8fdd36ceb2c2
Henry Davies <hdavies999@outlook.com>
2026-04-22 11:31:48 +0800

    Converted html pages to Jinja templates and reorganised app

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('prompt.html')
+
+@main.route('/browse')
+def browse():
+    return render_template('browse.html')
+
+@main.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')

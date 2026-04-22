aa607d4aaa3209f0f2b18bf14eab3aff499ff47c
Henry Davies <hdavies999@outlook.com>
2026-04-22 15:35:02 +0800

    Created a .sh equivalent to start.bat for mac/linuz users

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+    source venv/bin/activate
+    echo "Installing dependencies..."
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
+fi
+
+python3 run.py

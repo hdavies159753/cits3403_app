a0e1aa26eb118e4bdf230899e29a5222e62e5b27
Oscar_0103 <oscarchanhaha@gmail.com>
2026-05-06 01:03:01 +0800

    adding a function for submitting the drawing.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
+from app.submitting import submit_and_save
 
 main = Blueprint('main', __name__)
 
@@ -21,3 +22,12 @@ def login():
 @main.route('/drawing')
 def drawing():
     return render_template('drawing.html')
+
+@main.route('/submit_drawing', methods=['POST'])
+def submit_drawing():
+    data = request.get_json()
+    success, message = submit_and_save(data)
+    return jsonify({
+        "success": success, 
+        "message" : message
+    })

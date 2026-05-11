317c8ae52e31fefcaa6779beeb892e4ed8ee8959
Henry Davies <hdavies999@outlook.com>
2026-05-11 20:41:34 +0800

    Updated config/requirements/gitignore

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
 
-    SECRET_KEY = "dev-secret-key"
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False

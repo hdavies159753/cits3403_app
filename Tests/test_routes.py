a2d95b3c7ba55b55f42ea1e286b03beb2fa182ca
Aidan Harding <aidanharding2012@gmail.com>
2026-05-12 18:48:56 +0800

    Changed testing to match lecture

---
+++ Tests/test_routes.py
@@ -0,0 +1,38 @@
+import unittest
+from app import create_app
+
+class RouteTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+
+    # ---- ROUTE TESTS ----
+
+    def test_index_route(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_browse_route(self):
+        response = self.client.get('/browse')
+        self.assertEqual(response.status_code, 200)
+
+    def test_leaderboard_route(self):
+        response = self.client.get('/leaderboard')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_route(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+    def test_drawing_route(self):
+        response = self.client.get('/drawing')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_contains_content(self):
+        response = self.client.get('/')
+        self.assertIn(b"<html", response.data)  # basic sanity check
+
+if __name__ == "__main__":
+    unittest.main()
